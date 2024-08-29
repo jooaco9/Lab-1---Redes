aafd8d017f245a3d4ec5a28fbfab46b01d154e8b
jooaco9 <joaquin.corbo@gmail.com>
2024-08-29 20:10:41 -0300

    agregue comentarios

--- libServer.py
+++ libServer.py
@@ -4,6 +4,8 @@ from jsonrpc import JSONRPC
 from socket import *
 
 class Server:
+    
+    # Constructor
     def __init__(self, host, port ):
       self.host = host
       self.port = port
@@ -15,6 +17,7 @@ class Server:
       # Enlazar el socket a la dirección y puerto 
       self.skt.bind((self.host ,self.port))
            
+    # Arrancar el server
     def serve(self):
       # Configurar el socket para escuchar conexiones
       self.skt.listen()     
@@ -25,18 +28,22 @@ class Server:
         print(f"Conexion aceptada de {addr}")
         thread = threading.Thread(target=self.handle_client, args=(client,))
         thread.start()
-       
+
+    # Agregar metodos 
     def add_method(self, method):
       name = method.__name__
       self.methods[name] = method
-        
+
+    # Manejar el cliente 
     def handle_client(self, client_socket):
       buffer = ""
-      client_socket.settimeout(5)
+      client_socket.settimeout(5) # Setteo de timeout
       while True:
         
         buffer=""
         while not "}" in buffer:
+
+          # Recibir request
           try:
             data = client_socket.recv(1024).decode("utf-8") 
             if not data:
@@ -50,23 +57,24 @@ class Server:
           break
         
         try:
-          request = json.loads(buffer) 
-          method = self.methods.get(request['method'])
+          request = json.loads(buffer) # Deserialización
+          method = self.methods.get(request['method']) # Obtener metodo
 
           if method:
             try:
-              result = method(*request['params']) 
+              result = method(*request['params']) # Aplicar metodo
               if 'id' in request:
                 response = JSONRPC.create_response(result, request['id'])
             except TypeError as e:
-              response = JSONRPC.invalid_params(request['id'], str(e))
+              response = JSONRPC.invalid_params(request['id'], str(e)) # Error de parametros
           else:
             id = request.get('id')
-            response = JSONRPC.method_not_found(request['id'] if id else None)
+            response = JSONRPC.method_not_found(request['id'] if id else None) # Error de metodo no encontrado
             
           if 'id' in request:
-            client_socket.sendall(json.dumps(response).encode())
+            client_socket.sendall(json.dumps(response).encode()) # Enviar respuesta
             
+        # Error de formato JSON
         except json.JSONDecodeError:
           response = JSONRPC.invalid_request()
           client_socket.sendall(json.dumps(response).encode())
