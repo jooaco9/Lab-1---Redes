1faceb0e9385a4d6a36b3880fdb10bb911cba21c
jooaco9 <joaquin.corbo@gmail.com>
2024-08-29 19:51:55 -0300

    cambios para visualizar las llamadas

--- client.py
+++ client.py
@@ -6,9 +6,12 @@ if conn:
   print("Conexión exitosa.")
 
   # si es notificacion poner -> notify = true
-  print(f"Resultado de la resta 9 - 6 = {conn.resta(9, 6, notify = False)}")
-  print(f"Resultado de la resta 9 - 19 = {conn.resta(9, 19, notify = False)}")
-  print(f"Resultado de la multiplicacion 9 * 9 = {conn.multiplicar(9, 9, notify = False)}")
-  print(f"Resultado de la suma 23 + 7 = {conn.suma(23, 7, notify = False)}")
+  print(conn.resta(9, 6, notify = False))
+  print(conn.resta(9, 19, notify = False))
+  print(conn.hola(4, 3, notify = True))
+  print(conn.multiplicar(notify = False))
+  print(conn.suma(23, 7, notify = False))
+  print(conn.multipliar(3, -1, notify=True))
+  print(conn.suma(9, 9, 9,))
   conn.close()
   print("Cerrando conexión...")
